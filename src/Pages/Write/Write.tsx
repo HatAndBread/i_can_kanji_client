d605c25807850690e584bce22b203bbabb54c428
Joshua Hume <valencelectron@hotmail.com>
2021-04-18 23:03:51 +0900

    added p5

--- src/Pages/Write/Write.tsx
+++ src/Pages/Write/Write.tsx
@@ -1,8 +1,25 @@
+import { useEffect, useRef } from 'react';
+import P5 from 'p5';
+
 const Write = (): JSX.Element => {
+  const canvasRef = useRef<HTMLDivElement>(null);
+  const sketch = (p) => {
+    console.log(p);
+    p.setup = () => {
+      p.createCanvas(300, 200);
+    };
+    p.draw = () => {
+      p.background('orangered');
+      p.ellipse(150, 100, 100, 100);
+    };
+  };
+  useEffect(() => {
+    new P5(sketch, canvasRef.current);
+  }, []);
   return (
     <div>
       Writing practice page!
-      <div></div>
+      <div ref={canvasRef}></div>
     </div>
   );
 };
