58d4346ea20e500c4c7f1e39705e1196af9fd9db
Joshua Hume <valencelectron@hotmail.com>
2021-04-19 13:37:59 +0900

    drawing logic started

--- src/Pages/Write/Write.tsx
+++ src/Pages/Write/Write.tsx
@@ -3,17 +3,33 @@ import P5 from 'p5';
 
 const Write = (): JSX.Element => {
   const canvasRef = useRef<HTMLDivElement>(null);
-  const sketch = (p) => {
-    console.log(p);
-    p.setup = () => {
-      p.createCanvas(300, 200);
-    };
-    p.draw = () => {
-      p.background('orangered');
-      p.ellipse(150, 100, 100, 100);
-    };
-  };
   useEffect(() => {
+    const sketch = (p) => {
+      let lastX = null;
+      let lastY = null;
+      console.log(p);
+      p.setup = () => {
+        p.createCanvas(300, 200);
+        p.background('white');
+      };
+      p.draw = () => {
+        if (p.mouseIsPressed) {
+          if (lastX && lastY) {
+            p.line(lastX, lastY, p.mouseX, p.mouseY);
+            lastX = p.mouseX;
+            lastY = p.mouseY;
+          } else {
+            lastX = p.mouseX;
+            lastY = p.mouseY;
+            p.line(p.mouseX, p.mouseY, p.mouseX, p.mouseY);
+          }
+        }
+      };
+      p.mouseReleased = () => {
+        lastX = null;
+        lastY = null;
+      };
+    };
     new P5(sketch, canvasRef.current);
   }, []);
   return (
