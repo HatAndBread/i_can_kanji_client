cdb158648878a13b6305d4c9bd28b9bf428ce254
Joshua Hume <valencelectron@hotmail.com>
2021-04-20 17:01:12 +0900

    more canvas

--- src/Pages/Write/Write.tsx
+++ src/Pages/Write/Write.tsx
@@ -5,23 +5,28 @@ import Image from '../../Components/Image';
 const Write = (): JSX.Element => {
   const [width, setWidth] = useState(300);
   const [height, setHeight] = useState(300);
+  const [canvasCtx, setCanvasCtx] = useState<CanvasRenderingContext2D | null | undefined>();
   const [lastX, setLastX] = useState<null | number>(null);
   const [lastY, setLastY] = useState<null | number>(null);
+  const [currX, setCurrX] = useState<null | number>(null);
+  const [currY, setCurrY] = useState<null | number>(null);
   const [pointerDown, setPointerDown] = useState(false);
   const [rect, setRect] = useState<any>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   useEffect(() => {
-    const canvasContext = canvasRef.current?.getContext('2d');
-    if (canvasContext) {
-      canvasContext.fillStyle = '#ffffff';
-      canvasContext.fillRect(0, 0, width, height);
+    const newCtx = canvasRef.current?.getContext('2d');
+    setCanvasCtx(newCtx);
+    if (newCtx) {
+      newCtx.fillStyle = '#ffffff';
+      newCtx.fillRect(0, 0, width, height);
       setRect({ x: canvasRef.current?.getBoundingClientRect().x, y: canvasRef.current?.getBoundingClientRect().y });
     }
   }, [height, width]);
 
   useEffect(() => {
-    console.log(rect);
-  }, [rect]);
+    if (canvasCtx) {
+    }
+  }, [currX, currY, canvasCtx]);
 
   return (
     <div>
